269c286f4e0066c8c1862151c3bbb094fb1257d7
Vijay Pratap Singh <vijaypratap12121999@gmail.com>
2023-07-18 16:45:37 +0530

    Initial Commit

---
+++ dbconfig.js
@@ -0,0 +1,14 @@
+const config={
+    user:'foo',
+    password:'foo',
+    server:"MCNSolutions",
+    database:'Test',
+    options:{
+        trustedConnection:true,
+        enableArithPort:true,
+        instanceName:'SQLServer',
+
+    },
+    Port:1433
+}
+module.exports=config;
